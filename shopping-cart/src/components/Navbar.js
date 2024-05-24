9efeba7ac1d3d6cb2683be25aec6e5ff09282231
cynthiak-creator <cynthiakamuzembe21@gmail.com>
2024-05-24 14:59:35 +0200

    .

--- shopping-cart/src/components/Navbar.js
+++ shopping-cart/src/components/Navbar.js
@@ -3,14 +3,13 @@ import{Button, Container, Navbar,Modal} from 'react-bootstrap'
 function NavbarComponent() {
 
     return(
-        <Navbar bg="dark" variant="dark">
-            <Container>
+        <Navbar expand="sm">
                 <Navbar.Brand href="/">Ecommerce Store</Navbar.Brand>
                 <Navbar.Toggle />
                 <Navbar.Collapse className="justify-content-end">
-                    <button>Car 0 Items</button>
+                    <button>Cart 0 Items</button>
                     </Navbar.Collapse>
-            </Container>
+            
         </Navbar>
     )
 }
