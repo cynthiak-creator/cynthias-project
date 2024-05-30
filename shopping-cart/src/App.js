440855fb2f2612d1f09ff75fc35c61492f23bd9e
cynthiak-creator <cynthiakamuzembe21@gmail.com>
2024-05-30 22:12:27 +0200

    updating js

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavbarComponent from './Navbar';
+import  NavbarComponent from "./components/Navbar";
+import {Container} from 'react-bootstrap';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 function App() {
   return (
+    <Container>
     <NavbarComponent>
 
     </NavbarComponent>
+    </Container>
   );
 }
 export default App;
